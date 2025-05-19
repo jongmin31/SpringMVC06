947b21ac5b28c51f6b26469b7dc85102ff5084b4
lgdxschool <lgdxschool@192.168.219.121>
2025-05-19 12:32:31 +0900

    dd

--- SpringMVC06/src/main/java/com/lgdx/controller/BoardController.java
+++ SpringMVC06/src/main/java/com/lgdx/controller/BoardController.java
@@ -15,55 +15,53 @@ import com.lgdx.service.BoardService;
 @Controller
 public class BoardController {
  
- @Autowired
- private BoardService service;
+    @Autowired
+    private BoardService service;
 
- @GetMapping("/boardList.do")
- public String boardList(Model model) {
-  List<Board> list = service.boardList();
-  System.out.println(list.toString());
-  model.addAttribute("list", list);
-  return "boardList";
- }
- 
- @GetMapping("/boardContents.do")
- public String boardContents(@RequestParam("idx") Long idx, Model model) {
-	 
-	 service.boardCount(idx);
-	 
-	 Board vo = service.boardContents(idx);
-	 model.addAttribute("vo", vo);
-	 model.addAttribute("ln","\n");
-	 return "boardContents";
- }
- @GetMapping("/boardInsert.do")
- public String boardInsert() {
-	 return "boardInsert";
- }
- 
- @PostMapping("/boardInsert.do")
- public String boardInsert(Board vo) {
-	 service.boardInsert(vo);
-	 return "redirect:/boardList.do";
- }
- @GetMapping("/boardDelete.do")
- public String boardDelete(@RequestParam("idx") Long idx) {
-	 service.boardDelete(idx);
-	 return "redirect:/boardList.do";
- }
- 
- @GetMapping("/boardUpdate.do")
- public String boardUpdate(@RequestParam("idx") Long idx, Model model) {
-	 Board vo = service.boardContents(idx);
-	 model.addAttribute("vo",vo);
-	 return "boardUpdate";
- }
- 
- @PostMapping("/boardUpdate.do")
- public String boardUpdate(Board vo) {
-	 service.boardUpdate(vo);
-	 return "redirect:/boardList.do";
- }
- 
- 
+    @GetMapping("/boardList.do")
+    public String boardList(Model model) {
+        List<Board> list = service.boardList();
+        System.out.println(list.toString());
+        model.addAttribute("list", list);
+        return "boardList";
+    }
+
+    @GetMapping("/boardContents.do")
+    public String boardContents(@RequestParam("idx") Long idx, Model model) {
+        service.boardCount(idx);
+        Board vo = service.boardContents(idx);
+        model.addAttribute("vo", vo);
+        model.addAttribute("ln", "\n");
+        return "boardContents";
+    }
+
+    @GetMapping("/boardInsert.do")
+    public String boardInsert() {
+        return "boardInsert";
+    }
+
+    @PostMapping("/boardInsert.do")
+    public String boardInsert(Board vo) {
+        service.boardInsert(vo);
+        return "redirect:/boardList.do";
+    }
+
+    @GetMapping("/boardDelete.do")
+    public String boardDelete(@RequestParam("idx") Long idx) {
+        service.boardDelete(idx);
+        return "redirect:/boardList.do";
+    }
+
+    @GetMapping("/boardUpdate.do")
+    public String boardUpdate(@RequestParam("idx") Long idx, Model model) {
+        Board vo = service.boardContents(idx);
+        model.addAttribute("vo", vo);
+        return "boardUpdate";
+    }
+
+    @PostMapping("/boardUpdate.do")
+    public String boardUpdate(Board vo) {
+        service.boardUpdate(vo);
+        return "redirect:/boardList.do";
+    }
 }
--- SpringMVC06/src/main/java/com/lgdx/controller/MemberController.java
+++ SpringMVC06/src/main/java/com/lgdx/controller/MemberController.java
@@ -4,75 +4,51 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lgdx.entity.Member;
 import com.lgdx.service.MemberService;
 
 @Controller
 public class MemberController {
-	
-	@Autowired
-	private MemberService service;
-	
-	@GetMapping("/login.do")
-	public String login() {
-		return "login";
-	}
-	
-	@PostMapping("/login.do")
-	public String login(Member vo, HttpSession session) {
-		Member info = service.login(vo);
-		System.out.println(info);
-		
-		if(info!=null) {
-			session.setAttribute("info", info);
-		}
-		return "redirect:/boardList.do";
-	}
-	
-	@GetMapping("/join.do")
-	public String join() {
-		return "join";
-	}
-	
-	@PostMapping("/join.do")
-	public String join(Member vo) {
-		service.join(vo);
-		return "redirect:/boardList.do";
-	}
-	
-	@GetMapping("/logout.do")
-	public String logout(HttpSession session) {
-		session.removeAttribute("info");
-		session.invalidate();
-		return "redirect:/boardList.do";
-	}
-//	
-//	@GetMapping("/memberInsert.do")
-//	public String memberInsert() {
-//		return "memberInsert";
-//	}
-//	
-//	@PostMapping("/memberInsert.do")
-//	public String memberInsert(Member vo) {
-//		service.memberInsert(vo);
-//		return "redirect:/boardList.do";
-//	}
-//	
-//	@GetMapping("/memberDelete.do")
-//	public String memberDelete(@RequestParam("id") String id) {
-//		service.memberDelete(id);
-//		return "redirect:/boardList.do";
-//	}
-//	
-//	@GetMapping("/memberUpdate.do")
-//	public String memberUpdate(@RequestParam("id") String id, Model model) {
-//		Member vo = service.memberList(id);
-//		model.addAttribute("vo",vo);
-//		return "redirect:/boardList.do";
-//	}
+
+    @Autowired
+    private MemberService service;
+
+    @GetMapping("/login.do")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login.do")
+    public String login(Member vo, HttpSession session) {
+        Member info = service.login(vo);
+        System.out.println(info);
+
+        if (info != null) {
+            session.setAttribute("info", info);
+        }
+        return "redirect:/boardList.do";
+    }
+
+    @GetMapping("/join.do")
+    public String join() {
+        return "join";
+    }
+
+    @PostMapping("/join.do")
+    public String join(Member vo) {
+        service.join(vo);
+        return "redirect:/boardList.do";
+    }
+
+    @GetMapping("/logout.do")
+    public String logout(HttpSession session) {
+        session.removeAttribute("info");
+        session.invalidate();
+        return "redirect:/boardList.do";
+    }
+
+    // 미사용 멤버 관리 기능은 주석 처리 유지 (필요시 사용 가능)
 }
